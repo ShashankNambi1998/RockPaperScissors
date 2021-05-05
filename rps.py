5e241ef1edd20386e5e250c7e08bf0321eebda2f
Shashank Nambi <59937058+ShashankNambi1998@users.noreply.github.com>
2021-05-04 21:07:37 -0500

    Finished Basic Logic, need to fix lack of randomized computer choice

--- rps.py
+++ rps.py
@@ -1,11 +1,39 @@
 import random
 
-user_action = input("Please Enter Rock, Paper or Scissors: ")
-possible_responses = ['Rock', 'Paper', 'Scissors']
+loop = True
 
-computer_response = random.choice(possible_responses)
+while loop == True:
+    user_action = input("Please Enter Rock, Paper or Scissors: ")
+    possible_responses = ['Rock', 'Paper', 'Scissors']
 
-print(f"You chose {user_action}, the computer chose {computer_response}")
+    computer_response = random.choice(possible_responses)
+
+    print(f"You chose {user_action}, the computer chose {computer_response}")
+
+    if user_action == computer_response:
+        print("Same Response, No Winner")
+
+    if user_action == "Rock":
+        if computer_response == "Scissors":
+            print("You Win!")
+        elif computer_response == "Paper":
+            print("You Lose :(")
+    if user_action == "Scissors":
+        if computer_response == "Paper":
+            print("You Win!")
+        elif computer_response == "Rock":
+            print("You Lose :(")
+    if user_action == "Paper":
+        if computer_response == "Rock":
+            print("You Win!")
+        elif computer_response == "Scissors":
+            print("you Lose :(")
+
+    loop_condition = input("Would you like to keep playing? Yes/No ")
+
+    if loop_condition != "Yes":
+        loop = False
+
 
 
 
